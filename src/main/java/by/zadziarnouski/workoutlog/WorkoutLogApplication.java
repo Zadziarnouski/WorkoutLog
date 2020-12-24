56a2a1a538176f763b6604405be7e051fb04c3bf
taras <taras.zadziarnouski@gmail.com>
2020-12-24 12:20:50 +0300

    Created empty project

---
+++ src/main/java/by/zadziarnouski/workoutlog/WorkoutLogApplication.java
@@ -0,0 +1,13 @@
+package by.zadziarnouski.workoutlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkoutLogApplication.class, args);
+    }
+
+}
